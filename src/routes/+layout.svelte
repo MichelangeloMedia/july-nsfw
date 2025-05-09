6329254320c0f693a7945181745806dbb651176b
Michelangelo Media <losjuegosdeaxl@gmail.com>
2025-05-09 13:04:45 -0300

    Inject Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
-    let { children } = $props();
+    import { injectAnalytics } from '@vercel/analytics';
     import "../app.css";
+    
+    let { children } = $props();
+    injectAnalytics();
+
   </script>
   {@render children()}
+
+
+
+
+
+
